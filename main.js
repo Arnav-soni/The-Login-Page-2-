cee25fcaf4159c48467c2fa4963a66daa8d3ff4b
Arnav-soni <73822785+Arnav-soni@users.noreply.github.com>
2021-03-01 15:18:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function login() {
+    window.location = "quiz_game_page.html";
+}
+
+function twitter() {
+    window.alert("Our site Is on Maintance so Twitter And Facebook Sign In Option Won't be Working  . We are sorry for that. Have A Nice Day (^◡^ ).");
+}
+function facebook() {
+    window.alert("Our site Is on Maintance so Twitter And Facebook Sign In Option Won't be Working  . We are sorry for that. Have A Nice Day (^◡^ ).");
+}
+    
